8fdfe9bba1b393fdde4353e6e69dafef2f7626e1
Nathan Geater <78389584+nathangeater@users.noreply.github.com>
2022-04-02 19:22:08 -0500

    Add tile set and a tiled level

---
+++ assets/tileSet/Project2Tiles.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Project2Tiles" tilewidth="128" tileheight="128" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="stone_tile.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Corners/bl_corner.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Corners/br_corner.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Corners/tl_corner.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Corners/tr_corner.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Flats/bottom_wall.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Flats/left_wall.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Flats/right_wall.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Flats/top_wall.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Shadows/bl_shadow.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Shadows/bottom_shadow.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Shadows/br_shadow.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Shadows/left_shadow.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Shadows/right_shadow.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Shadows/tl_shadow.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Shadows/top_shadow.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../../../../../Downloads/Walls-20220402T220237Z-001/Walls/Shadows/tr_shadow.png"/>
+ </tile>
+</tileset>
